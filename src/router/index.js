4add429a7fb82fc04e20bd21f46cc5f040f981fb
Lungabooi <Lungalucasbooi@gmail.com>
2022-10-21 16:03:44 +0200

    one page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddProject from '../views/AddProject'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/AddProject',
+    name: 'AddProject',
+    component: AddProject
   },
   
 ]
---
+++ src/views/AddProject.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <input type="text" v-model="title" placeholder="title" required>
+        <textarea type="text" v-model="details" placeholder="details" required> </textarea>
+        <button> Add Project</button>
+
+    </form>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title:'',
+            details:''
+        }
+    }, 
+    methods: {
+        handleSubmit() {
+        let project = {
+            title: this.title,
+            details: this.details,
+            complete: false
+        }
+        fetch('http://localhost:3000/projects', {
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(project)
+        })
+        }
+       
+    }
+
+}
+</script>
+
+<style>
+
+</style>
