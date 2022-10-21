b66c8a77a5d407bfecd4309d9cb610919d9ef326
Lungabooi <Lungalucasbooi@gmail.com>
2022-10-21 12:22:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+body{
+  background-color:rgb(232, 215, 215) ;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #2c3e50;
+
+}
+
+</style>
